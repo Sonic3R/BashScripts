2acccce16110a2ddc6b5ad0bb21c4160d5efeebd
Teodorescu Mihail <mihail.teodorescu>
2020-04-13 11:39:07 +0300

    create machine

--- azure_scripts/create_machine.ps1
+++ azure_scripts/create_machine.ps1
@@ -1,4 +1,5 @@
 ﻿az login
+#az group create --name UbuntuRG --location westus
 az deployment group create --name MyMachineDeployment --resource-group UbuntuRG --template-file template.json --parameters parameters.json
 
 
