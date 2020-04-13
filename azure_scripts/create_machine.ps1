7dde8e4cfa597bac5a5ccaf25e422965f8712eaa
Teodorescu Mihail <mihail.teodorescu>
2020-04-13 17:22:12 +0300

    add security in parameters json

--- azure_scripts/create_machine.ps1
+++ azure_scripts/create_machine.ps1
@@ -17,7 +17,7 @@ $content | Out-File parameters1.json
 az login
 az group create --name UbuntuRG --location westus
 az deployment group create --name MyMachineDeployment --resource-group UbuntuRG --template-file template.json --parameters parameters1.json
-az network nsg rule create --name PortFTP --nsg-name ubuntuvm707 --resource-group UbuntuRG --access Allow --direction Inbound --protocol '*' --priority 310 --destination-port-ranges '20-21;40000-50000'
+#az network nsg rule create --name PortFTP --nsg-name ubuntuvm707 --resource-group UbuntuRG --access Allow --direction Inbound --protocol '*' --priority 310 --destination-port-ranges '20-21;40000-50000'
 
 Remove-Item parameters1.json
 
