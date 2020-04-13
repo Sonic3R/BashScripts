7f281bb60d7a1896e7bb10435d1ef2429e0c04fa
Teodorescu Mihail <mihail.teodorescu>
2020-04-13 12:12:56 +0300

    argumentize powershell script

--- azure_scripts/create_machine.ps1
+++ azure_scripts/create_machine.ps1
@@ -1,7 +1,24 @@
-﻿az login
-#az group create --name UbuntuRG --location westus
-az deployment group create --name MyMachineDeployment --resource-group UbuntuRG --template-file template.json --parameters parameters.json
+﻿if($args.Length -le 1) {
+   Write-Host "Invalid args"
+   return;
+}
+   
+[string]$user=$args[0]
+[string]$password=$args[1]
 
+Copy-Item parameters.json parameters1.json
+$content = Get-Content parameters1.json
+
+$content = $content -replace "@@user", $user
+$content = $content -replace "@@pass", $password
+
+$content | Out-File parameters1.json
+
+az login
+az group create --name UbuntuRG --location westus
+az deployment group create --name MyMachineDeployment --resource-group UbuntuRG --template-file template.json --parameters parameters1.json
+
+Remove-Item parameters1.json
 
 #az group delete --name UbuntuRG
 #az group delete --name NetworkWatcherRG
