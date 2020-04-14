c6d1f2156afbb683490471d5ecb5688139f7d2f5
Teodorescu Mihail <mihail.teodorescu>
2020-04-14 15:21:28 +0300

    same

--- bash_scripts/bdinfo_local.sh
+++ bash_scripts/bdinfo_local.sh
@@ -88,10 +88,10 @@ echo Generate bd info
 if [[ -f "${outputftp}/${foldername}.txt" ]]; then
   read -e -p "Generate BDInfo again ? [Y/n]" readbdinfo
   if [[ $readbdinfo == "y" || $readbdinfo == "Y" || $readbdinfo == "" ]]; then
-    dotnet $mntdisk/bdinfo/BDInfo.dll -p $mntdisk/$foldername -r $outputftp -o "${foldername}.txt"
+    dotnet $mntdisk/bdinfo/BDInfo.dll -p $mntdisk/$foldername -r $outputftp -o "${foldername}.txt" -z false
   fi
 else
-  dotnet $mntdisk/bdinfo/BDInfo.dll -p $mntdisk/$foldername -r $outputftp -o "${foldername}.txt"
+  dotnet $mntdisk/bdinfo/BDInfo.dll -p $mntdisk/$foldername -r $outputftp -o "${foldername}.txt" -z false
 fi
 
 if [[ $? -eq 1 ]]; then
