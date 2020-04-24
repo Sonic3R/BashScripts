5db570318edbea46f859090d67312519b282e26c
Teodorescu Mihail <mihail.teodorescu>
2020-04-24 07:56:00 +0300

    can add multiple operations

--- bash_scripts/aio.sh
+++ bash_scripts/aio.sh
@@ -1,15 +1,14 @@
-args=("$@")
-blurayfolder=${args[0]}
-
-clear
-
 cd /home
+for blurayfolder in "$@"
+do
+  clear
+    
+  iso=$(find $blurayfolder -name *.iso)
+  if [[ $iso != "" ]]; then
+    echo $iso
+    dotnet ftpuser/bdextract/BDExtractor.dll -p $blurayfolder/$iso
+    rm $blurayfolder/$iso
+  fi
 
-iso=$(find $blurayfolder -name *.iso)
-if [[ $iso != "" ]]; then
-  echo $iso
-  dotnet ftpuser/bdextract/BDExtractor.dll -p $blurayfolder/$iso
-  rm $blurayfolder/$iso
-fi
-
-bash bdscript_with_ss.sh $blurayfolder
+  bash bdscript_with_ss.sh $blurayfolder
+done
