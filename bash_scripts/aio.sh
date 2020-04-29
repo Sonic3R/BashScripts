3974b2063c86ae160ab7f69b44f04f2da9b249f0
Teodorescu Mihail <mihail.teodorescu>
2020-04-29 08:57:21 +0300

    mount iso for 3D, generate report and then umount device

--- bash_scripts/aio.sh
+++ bash_scripts/aio.sh
@@ -14,10 +14,18 @@ do
     if [[ $iso != *"3D"* ]];then
       dotnet /home/ftpuser/bdextract/BDExtractor.dll -p $iso -o $blurayfolderitem
       rm $iso
-    else
-      blurayfolderitem=$iso
-    fi
-  fi  
 
-  bash /home/bdscript_with_ss.sh $blurayfolderitem
+      bash /home/bdscript_with_ss.sh $blurayfolderitem
+    else      
+      mkdir /media/iso
+      mount -o loop $iso /media/iso
+  
+      bash /home/bdscript_with_ss.sh /media/iso
+
+      umount /media/iso
+      rmdir /media/iso
+    fi
+  else
+    bash /home/bdscript_with_ss.sh $blurayfolderitem
+  fi
 done
