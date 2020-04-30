0990d83476ed5c381c7f9cc67e0190834b1a2dbb
Teodorescu Mihail <mihail.teodorescu>
2020-04-30 12:03:10 +0300

    quote argument because it may contains space

--- bash_scripts/aio.sh
+++ bash_scripts/aio.sh
@@ -1,25 +1,25 @@
 for blurayfolderitem in "$@"
 do
-  echo Processing $blurayfolderitem
+  echo Processing "$blurayfolderitem"
   
-  iso=$(find $blurayfolderitem -name *.iso)
+  iso=$(find "$blurayfolderitem" -name *.iso)
 
-  if [[ $iso == "" ]]; then
-    iso=$(find $blurayfolderitem -name *.ISO)
+  if [[ "$iso" == "" ]]; then
+    iso=$(find "$blurayfolderitem" -name *.ISO)
   fi
   
-  if [[ $iso != "" ]]; then
-    echo $iso
+  if [[ "$iso" != "" ]]; then
+    echo "$iso"
 
-    if [[ $iso != *"3D"* ]];then
-      dotnet /home/ftpuser/bdextract/BDExtractor.dll -p $iso -o $blurayfolderitem
-      rm $iso
+    if [[ "$iso" != *"3D"* ]];then
+      dotnet /home/ftpuser/bdextract/BDExtractor.dll -p "$iso" -o "$blurayfolderitem"
+      rm "$iso"
 
-      bash /home/bdscript_with_ss.sh $blurayfolderitem
+      bash /home/bdscript_with_ss.sh "$blurayfolderitem"
     else      
-      foldername=$(basename $blurayfolderitem)
+      foldername=$(basename "$blurayfolderitem")
       mkdir /media/$foldername
-      mount -o loop $iso /media/$foldername
+      mount -o loop "$iso" /media/$foldername
   
       bash /home/bdscript_with_ss.sh /media/$foldername
 
@@ -27,6 +27,6 @@ do
       rmdir /media/$foldername
     fi
   else
-    bash /home/bdscript_with_ss.sh $blurayfolderitem
+    bash /home/bdscript_with_ss.sh "$blurayfolderitem"
   fi
 done
