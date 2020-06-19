bfb2661912023e0be9afc3786849f0ebb609e79e
Mihail Teodorescu <mihail.teodorescu@cegeka.com>
2020-06-19 16:43:57 +0300

    move bdmv to right location

--- bash_scripts/autoupload.sh
+++ bash_scripts/autoupload.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+getbdmvfolder() {
+  current="$1"
+  bdmv=$(find "$current" -iname bdmv -type d)
+
+  if [[ $bdmv == "" ]]; then
+    echo ""
+  else
+    echo "$bdmv"
+  fi
+}
+
 foldertolookin="$1"
 movetopath="/home/sonic3r/torrents/rtorrent/bluray"
 
@@ -17,6 +28,16 @@ folder=$(find "$foldertolookin" -mindepth 1 -maxdepth 1 -type d | head -n1)
 if [[ $folder == "" ]]; then
   echo "Nothing to process"
   exit
+fi
+
+bdmv=$(getbdmvfolder "$folder")
+if [[ $bdmv != "" ]]; then
+  prevdir=$(dirname $bdmv)
+
+  if [[ $prevdir != $folder ]]; then
+    mv "${prevdir}/*" "${folder}/"
+    rm -rf $prevdir
+  fi
 fi
 
 mv $folder "$movetopath/"
