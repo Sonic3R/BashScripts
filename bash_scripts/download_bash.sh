7cb1d86ac72f8b0e56222a413a1fbea7ffc8939b
Mihail Teodorescu <mihail.teodorescu@cegeka.com>
2020-07-14 15:30:13 +0300

    download bash

---
+++ bash_scripts/download_bash.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+path="$1"
+
+if [[ $path == "" ]]; then
+  path="/home/sonic3r/myscripts";
+fi
+
+my_array=(bdscript.sh bluray_autoupload.sh cron.sh extract_rar.sh ffmpeg.sh bluray_aio.sh create_torrent.sh extract_iso.sh ffmpeg_base.sh upload.sh)
+url="https://raw.githubusercontent.com/Sonic3R/Scripts/ubuntu_hetzner/bash_scripts"
+
+for i in "${my_array[@]}"; 
+do
+  full_url="${url}/${i}"
+  full_path="${path}/${i}"
+
+  if [[ -f $full_path ]]; then
+    rm $full_path
+  fi
+
+  wget $full_url $full_path
+
+  chmod +x $full_path
+done
