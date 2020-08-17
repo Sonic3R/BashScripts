0cf793f39258d3ea274bd95969b89e2c8033924c
Sonic3R <teodorescumihail2008@gmail.com>
2020-08-17 12:17:18 +0300

    Create auto_rclone.sh

---
+++ rclone/auto_rclone.sh
@@ -0,0 +1,23 @@
+lookin="/home/r0gu3ptm/rtorrent/download/bluray/"
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+blurays=$(find "$lookin" -mindepth 1 -maxdepth 1 -type d)
+
+if [[ $blurays == "" || $blurays == $lookin ]]; then
+   exit 1
+fi
+
+for bluray in $blurays; do
+  busystatus=$(lsof "$bluray"/*)
+
+  if [[ $busystatus != "" ]]; then
+    echo "$bluray is busy. Skipping"
+    continue
+  fi
+
+  bash execute_rclone.sh "$bluray"
+done
+
+IFS=$SAVEIFS
